03981e33b270683a6a3e300c7e742a1171782acc
derrickSP <57939887+derrickSP@users.noreply.github.com>
2019-12-31 11:29:44 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,7 +17,46 @@ $(document).ready(function() {
     container: "#third-layer",
     sidebarInner: ".portfolio-item"
   });
+
+  $(".slick-container").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    easing: "linear",
+    infinite: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 786,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
 });
+
+function responsiveSlider(slider) {
+  var settings = {
+    dots: true,
+    arrows: false
+  };
+  if ($(window).width() >= 901) {
+    if (slider.hasClass("slick-initialized")) {
+      slider.slick("unslick");
+    }
+    return;
+  } else if (!slider.hasClass("slick-initialized")) {
+    return slider.slick(settings);
+  }
+}
 
 $(window).resize(function() {
   $("#fyp-wrapper").stickySidebar({
